b05f1fe92cd36aabcdf94cffb62c2238ff2e5310
Sarygin Rodion <k1ll4h_tv3t@protonmail.com>
2026-07-18 08:21:24 +0700

    feat-11: change in component package docstring

--- component/component.go
+++ component/component.go
@@ -1,5 +1,7 @@
 // component package holds everything needed to manipulate with components.
 //
+// In ECS world components is a small data structure without logic that attached to entity. Entity is formed from one or multiple components.
+//
 // [ComponentStorage] is the main structure. It holds every component and can be used to set/get/remove/query components.
 // 
 // Uses sparse-set implementation

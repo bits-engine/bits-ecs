48bd5eefe26abb9b4f3ed84aa9d5b6605df61be6
Sarygin Rodion <k1ll4h_tv3t@protonmail.com>
2026-07-18 08:18:04 +0700

    feat-11: docstrings for logging

--- logging/logging.go
+++ logging/logging.go
@@ -1,3 +1,11 @@
+// logging package provides helper for creating new slog loggers and some keys for structured logging.
+//
+// Basically uses lazy evaluation. With it, user can define their logger by slog.SetDefault()
+//
+// # Usage
+//     var log = logging.New("my-origin")
+//     
+//     log.Get().Info("Hello world!")
 package logging
 
 import (
@@ -6,6 +14,7 @@ import (
 	"github.com/bits-engine/bits-ecs/common/lazyvalue"
 )
 
+// New creates new [lazyvalue.LazyValue] that gets [slog.Default] logger.
 func New(originName string) *lazyvalue.LazyValue[*slog.Logger] {
 	return lazyvalue.New(func() *slog.Logger {
 		 return slog.Default().With(KeyOrigin, originName)
